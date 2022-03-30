d4df1590c662c78ca0135896d27d2d7ee2d32ec0
KaterynaN <kateryna.od.ua@gmail.com>
2022-03-30 17:12:11 +0300

    Partly fix FontAwesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 import "@fortawesome/fontawesome-free/css/all"
-
+import "@fortawesome/fontawesome-free/js/all"
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
